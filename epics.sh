ff8ad9957b5340b50920d8a6175fd450bb082883
Johan Montorfano <me@johanmontorfano.com>
2025-01-27 06:55:21 +0100

    add: lua app launcher

---
+++ epics.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+cd $DIR
+lua ./src/main.lua $1
